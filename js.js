06ba6095f2c908c970fefaaf91d223108f5d6991
nel <zhanel.usonkulova@alatoo.edu.kg>
2025-10-08 10:35:59 +0600

    a

---
+++ js.js
@@ -0,0 +1,10 @@
+const score = 75;
+ if (score >= 90) {
+ console.log('Отлично');
+ } else if (score >= 60) {
+ console.log('Хорошо');
+ } else {
+ console.log('Нужно подтянуть');
+ }
+ const result = score >= 60 ? 'зачёт' : 'незачёт';
+  console.log(result);
